20761a7c47e0bc2ec2771cccdaad1a25788b0183
@slovacus <slovacus@gmail.com>
2021-02-12 10:15:33 -0500

    build: generate version patch (0.1.1)

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ module "main" {
   providers = {
     github = github
   }
-  source      = "git://github.com/hadenlabs/terraform-github-iam.git?ref=0.1.0"
+  source      = "git://github.com/hadenlabs/terraform-github-iam.git?ref=0.1.1"
   teams       = var.teams
   members     = var.members
   permissions = var.permissions
