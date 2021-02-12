7cf0ace67e298bd7f7e3852423b6e36b5b51ef7c
@slovacus <slovacus@gmail.com>
2021-02-11 21:34:15 -0500

    chore: implement action make docs terraform (#1)

--- provision/make/docs.mk
+++ provision/make/docs.mk
@@ -10,7 +10,13 @@ docs.help:
 	@echo ''
 	@echo '        docs.build                  Show mkdocs'
 	@echo '        docs.serve                  server Make documentation'
+	@echo '        docs.terraform             generated docs for terraform'
 	@echo ''
+
+docs.terraform:
+	$(call terraform-docs, ${TERRAFORM_README_FILE}, \
+			'This document gives an overview of variables used in the platform of the ${PROJECT}.', \
+			variables.tf)
 
 docs.build:
 	$(PIPENV_RUN) mkdocs build
