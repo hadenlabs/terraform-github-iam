2f7f8dfc9a097dedb98fc3fe5779803d0b2ad4d7
@slovacus <slovacus@gmail.com>
2021-02-12 10:11:35 -0500

    fix: attributes load github team (0.1.1)

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "github_team" "this" {
 
 resource "github_team_membership" "this" {
   count      = length(var.members)
-  team_id    = element(github_team.teams, index(github_team.teams.*.name, element(var.members, count.index).name)).id
+  team_id    = element(github_team.this, index(github_team.this.*.name, element(var.members, count.index).team)).id
   username   = element(var.members, count.index).username
   role       = element(var.members, count.index).role
   depends_on = [github_team.this]
@@ -15,7 +15,7 @@ resource "github_team_membership" "this" {
 
 resource "github_team_repository" "this" {
   count      = length(var.permissions)
-  team_id    = element(github_team.teams, index(github_team.teams.*.name, element(var.permissions, count.index).team)).id
+  team_id    = element(github_team.this, index(github_team.this.*.name, element(var.permissions, count.index).team)).id
   repository = element(var.permissions, count.index).repository
   permission = element(var.permissions, count.index).permission
   depends_on = [github_team.this, github_team_membership.this]
