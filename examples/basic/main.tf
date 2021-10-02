63fc1e3aa36f1191df9357e406ecbde39b910dbc
luismayta <luis@hadenlabs.com>
2021-10-02 07:52:04 -0500

    sample: implement basic (#15)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,8 +1,9 @@
 module "main" {
+  source = "git://github.com/hadenlabs/terraform-github-iam.git?ref=0.1.1"
   providers = {
     github = github
   }
-  source      = "git://github.com/hadenlabs/terraform-github-iam.git?ref=0.1.1"
+
   teams       = var.teams
   members     = var.members
   permissions = var.permissions
